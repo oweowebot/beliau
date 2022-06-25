720aca5be260f35f0d8ffd2da98bf5615a63a645
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2022-06-25 23:15:12 +0700

    Update anoboylast.js

--- plugins/anoboylast.js
+++ plugins/anoboylast.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
-    let res = await fetch('https://x-restapi.herokuapp.com/api/anoboy-last?apikey=BETA')
+    let res = await fetch('https://x-restapi.herokuapp.com/api/anoboy-last?apikey=xapi354')
     if (!res.ok) throw await res.text()
     let json = await res.json()
     let keqing = json.data.map((v, i) => `#${i + 1}. \n*Title:* ${v.title}\n*Info:* ${v.time}\n==============\n`).join('\n') 
