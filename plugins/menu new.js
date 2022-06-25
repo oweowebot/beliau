d4cf056790730075aa9c3b246cbd885838bdb214
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2022-06-25 18:18:05 +0700

    Update menu new.js

--- plugins/menu new.js
+++ plugins/menu new.js
@@ -2,10 +2,10 @@ let fs = require('fs')
 
 let handler = async (m, { conn, text }) => {
 let logo = fs.readFileSync(`./src/logo.jpg`)
+let lagu = fs.readFileSync(`./src/lagu.mp3`)
 let caption = `Welcome *${conn.getName(m.sender)}* to *_RhaynaBotz_*\n-Klik *_Menu_* untuk menampilkan list menu\n-Klik *_Group_* untuk link group official bot\n-Klik *_Rules_* untuk melihat rules _RhaynaBotz_\n\n *(bot tidak aktif 24 jam)*`
-let wm = global.wm
-m.reply(`*Sedang mengambil menu di kamar ${conn.getName(m.sender)}*`)
 await conn.send3ButtonImg(m.chat, logo, caption, wm, 'Menu', '.sihsvsudbsjagsb', 'Group', '.gerup', 'Rules', '.rules', m)
+await conn.reply(m.chat, lagu, m)
 }
 handler.command = /^menu$/i
 handler.tags = ['main']
