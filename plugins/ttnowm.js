d9f83dcecacdabce0c8ad8a28e3a3efa3154559b
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2022-06-26 00:33:09 +0700

    Update ttnowm.js

--- plugins/ttnowm.js
+++ plugins/ttnowm.js
@@ -5,7 +5,7 @@ let fs = require('fs')
 let handler = async(m, { conn, usedPrefix, text, command }) => {
     if (!text) throw `Harap masukkan URL sebagai teks \n\nContoh : ${usedPrefix + command} https://vm.tiktok.com/ZSd4HKgSw/?k=1`
     let res = await fetch(`https://api-kaysa.herokuapp.com/api/tiktok?url=${text}&apikey=KaysaS`)
-    if (!res.ok) throw await `${res.status} ${res.statusText}`
+    if (!res.ok) throw 'Error Website sedang down'
     let json = await res.json()
     await conn.sendFile(m.chat, json.medias[1].url, 'tt.mp4', `Title : ${json.title}`, m)
 }
