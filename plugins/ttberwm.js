2d853c947afc2de5ef0ea662e5c4100e3851d4e8
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2022-06-26 00:35:16 +0700

    Update ttberwm.js

--- plugins/ttberwm.js
+++ plugins/ttberwm.js
@@ -5,9 +5,9 @@ let fs = require('fs')
 let handler = async(m, { conn, usedPrefix, text, command }) => {
     if (!text) throw `Harap masukkan URL sebagai teks \n\nContoh : ${usedPrefix + command} https://vm.tiktok.com/ZSd4HKgSw/?k=1`
     let res = await fetch(`https://api-kaysa.herokuapp.com/api/tiktok?url=${text}&apikey=KaysaS`)
-    if (!res.ok) throw await `${res.status} ${res.statusText}`
+    if (!res.ok) throw 'Error Website sedang down'
     let json = await res.json()
-    await conn.sendFile(m.chat, json.medias[0].url, 'tt.mp4', `Title : ${json.title}`, m)
+    await conn.sendFile(m.chat, json.medias[0].url, 'tt.mp4', `Title : ${json.title}\n*ketik .tiktoknowm (link) untuk no watermark*`, m)
 }
 handler.help = ['tiktok *link*']
 handler.tags = ['downloader']
