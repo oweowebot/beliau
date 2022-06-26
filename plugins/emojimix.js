3f262a12826008ad414af6527600ce8ce9d1f381
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2022-06-26 23:44:12 +0700

    Update emojimix.js

--- plugins/emojimix.js
+++ plugins/emojimix.js
@@ -3,8 +3,8 @@ let {fetchJson} = require("../lib/myfunc")
 let fs = require("fs")
 
 let handler = async (m, { conn, text, args }) => {
-if (!args[0]) throw 'Contoh penggunaan:\n\n*.emojimix 🐷&😣*'
-   	let [emoji1, emoji2] = text.split`&`
+if (!args[0]) throw 'Contoh penggunaan:\n\n*.emojimix 🐷|😣*'
+   	let [emoji1, emoji2] = text.split`|`
 		let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
 		for (let res of anu.results) {
 		    let encmedia = await conn.sendImageAsSticker(m.chat, res.url, m, { packname: global.packname, author: global.author, categories: res.tags })
