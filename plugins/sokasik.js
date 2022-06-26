55cbd1fa475b2a1be4c6bf4baba8d8e5f9528104
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2022-06-26 15:32:29 +0700

    Update sokasik.js

--- plugins/sokasik.js
+++ plugins/sokasik.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 conn.sendFile(m.chat, 'https://telegra.ph/file/8c40e7aaf759253d85fe0.mp4', 'ok.mp4', 'Beliau Ini', m)
 }
 
-handler.customPrefix = /^(@6287831204077|@6285974899229)/i
+handler.customPrefix = /@6287831204077|@6285974899229/i
 handler.command = new RegExp
 
 module.exports = handler
