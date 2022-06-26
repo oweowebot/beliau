32a9dee33d34dd9a1b53bdb31b94034eb0a30048
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2022-06-27 00:37:27 +0700

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -1,11 +1,11 @@
-let scraper = require('@bochilteam/scraper')
-
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://www.facebook.com/alanwalkermusic/videos/277641643524720`
-  await m.reply('Loading...')
-  let res = await scraper.facebookdl(args[0])
-  let data = res.result.filter(v => /true/.test(v.isVideo) && /mp4/.test(v.ext))
-  await conn.sendFile(m.chat, data[1].url, '', `HD: ${data[0].url}`, m)
+let fetch = require('node-fetch')
+let fs = require('fs')
+let handler = async(m, { conn, usedPrefix, text, command }) => {
+    if (!text) throw `Harap masukkan URL\n\nContoh : .fb https://www.facebook.com/alanwalkermusic/videos/277641643524720`
+    let res = await fetch(`https://api-kaysa.herokuapp.com/api/fbdl?url=${text}&apikey=KaysaS`)
+    if (!res.ok) throw await `${res.status} ${res.statusText}`
+    let json = await res.json()
+    await conn.sendFile(m.chat, json.medias.url, 'kntl.mp4', `*Nih*`, m)
 }
 handler.help = ['fb'].map(v => v + ' <url>')
 handler.tags = ['downloader']
