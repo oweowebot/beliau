085ba82943dee40188bf40797afd76583cb6aea3
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2022-06-26 10:09:55 +0700

    Add files via upload

--- plugins/addprem.js
+++ plugins/addprem.js
@@ -1,22 +1,23 @@
+let fs = require('fs')
 let handler = async (m, { conn, text }) => {
 
+    const json = JSON.parse(fs.readFileSync('./src/premium.json'))
     let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
-    else who = m.chat
-    if (!who) throw `tag orangnya!`
-    if (global.prems.includes(who.split`@`[0])) throw 'dia udah premium!'
-    global.prems.push(`${who.split`@`[0]}`)
-    conn.reply(m.chat, `@${who.split`@`[0]} sekarang premium!`, m, {
-        contextInfo: {
-            mentionedJid: [who]
-        }
-    })
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '') + '@s.whatsapp.net'
+    else who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
+    if (json.includes(who.split`@`[0])) throw `Dia udah Premium`
+    json.push(`${who.split`@`[0]}`)
+    fs.writeFileSync('./src/premium.json', JSON.stringify(json))
+    m.reply(`@${who.split`@`[0]} antum sekarang premium bang`)
+
+    delete require.cache[require.resolve('../config')]
+    require('../config')
 
 }
 handler.help = ['addprem [@user]']
 handler.tags = ['owner']
 handler.command = /^(add|tambah|\+)prem$/i
 
-handler.rowner = true
+handler.owner = true
 
 module.exports = handler
