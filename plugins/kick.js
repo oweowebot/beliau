2bb4bc0085ca8fba27c108f3350f17879dc01714
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2022-06-28 23:21:09 +0700

    Update kick.js

--- plugins/kick.js
+++ plugins/kick.js
@@ -1,18 +1,29 @@
-let handler = async (m, { conn, args }) => {
-  let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
-  let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
-  let who
-  if (m.isGroup) who = m.mentionedJid[0]
-  else who = m.chat
-  if (!who) throw 'Tag salah satu lah'
-  for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
+let handler = async (m, { teks, conn, isOwner, isAdmin, args }) => {
+	if (!(isAdmin || isOwner)) {
+                global.dfail('admin', m, conn)
+                throw false
+                }
+  let ownerGroup = m.chat.split`-`[0] + "@s.whatsapp.net";
+  if(m.quoted){
+if(m.quoted.sender === ownerGroup || m.quoted.sender === conn.user.jid) return;
+let usr = m.quoted.sender;
+await conn.groupParticipantsUpdate(m.chat, [usr], "remove"); return;
 }
-handler.help = ['kick']
-handler.tags = ['Admin']
-handler.command = /^(kick)$/i
-handler.owner = false
-handler.admin = true
+  if (!m.mentionedJid[0]) throw `tag yang mau dikick`;
+  let users = m.mentionedJid.filter(
+    (u) => !(u == ownerGroup || u.includes(conn.user.jid))
+  );
+  for (let user of users)
+    if (user.endsWith("@s.whatsapp.net"))
+      await conn.groupParticipantsUpdate(m.chat, [user], "remove");
+};
+
+handler.help = ['kick @user']
+handler.tags = ['group']
+handler.command = /^(kic?k|remove|tendang|\-)$/i
+
+handler.group = true
 handler.botAdmin = true
-handler.limit = false
+handler.admin = true
 
 module.exports = handler
