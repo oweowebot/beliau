1f8c4f441f3a6bdda82610ac12ae6b3b914582e5
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2022-06-29 23:56:45 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -9,7 +9,7 @@ handler.before = async function (m, { isAdmin, isBotAdmin }) {
   if (chat.antiLink && isGroupLink && !isAdmin && !m.isBaileys && m.isGroup) {
     let thisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
     if (m.text.includes(thisGroup)) throw false // jika link grup itu sendiri gak dikick
-      if (!isBotAdmin) m.reply(` *「 ANTILINK 」* ${isAdmin ? "Admin mah bebas :'v" : `\n\nlink group terdeteksi dan ${global.namabot} bukan admin jadi tidak bisa ngekick!`}`)
+      if (!isBotAdmin) m.reply(` *「 ANTILINK 」* ${isAdmin ? "Admin mah bebas :'v" : `\n\nlink biru terdeteksi dan bot bukan admin jadi gabisa nge kick`}`)
     if (isBotAdmin) {
       m.reply(` *「 ANTILINK 」* \n\nLink Biru Terdeteksi, Lu Akan Di Kick Terimakasih`.trim())
       await this.delay(500)
