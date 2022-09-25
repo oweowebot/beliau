991bdcf6ac2b8940f0b40705433dba385ccb53ca
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2022-09-25 15:02:28 +0700

    Update gerup.js

--- plugins/gerup.js
+++ plugins/gerup.js
@@ -1,6 +1,5 @@
 let handler = async m => m.reply(`
-Masukin bot lu, biar tawuran antar bot
-https://chat.whatsapp.com/BgKpySEngapLoJ3IVNChBa`.trim()) // Tambah sendiri kalo mau
+Udah gada awkwok`.trim()) // Tambah sendiri kalo mau
 handler.command = /^gerup$/i
 handler.help = ['gerup']
 handler.tags = ['info']
