6a49f6c0b2674d607352a8f78220744c4967d6b0
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-01-05 18:37:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -66,21 +66,21 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = 'By'
-global.author = 'SillBotz'
+global.author = 'Dhkzbot'
 
 
 
 
 
 ig: '-'
-global.wm = 'SillBotz'
+global.wm = 'Dhkzbot'
 global.watermark = wm
 global.botdate = '╭──────────═┅═─────────\n│◪ ${time}\n┊◪ ${week} ${date}\n╰──────────═┅═─────────'
 global.media = 'https://telegra.ph/file/e001198ced5650ef53d47.jpg'
 global.instagram = '-'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text='
-global.wait = '_*tunggu sedang di proses...*_'
-global.eror = '_*Server Error*_'
+global.wait = '_*Loading ngab....*_'
+global.eror = '_*Server Error!*_'
 global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 
